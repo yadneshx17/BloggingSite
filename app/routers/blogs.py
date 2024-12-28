ede555920803c2c64916c9b9b69c854de6545737
yadneshx17 <yadneshtambe17@gmail.com>
2024-12-28 23:55:08 +0530

    Basic CRUD Operations

--- app/routers/blogs.py
+++ app/routers/blogs.py
@@ -1,12 +1,12 @@
 from ..models import Blog
 from sqlalchemy.orm import Session
 from app.database import get_db
-from fastapi import Depends
+from fastapi import Depends, HTTPException, Response, status
 from fastapi import APIRouter
 
 router = APIRouter()
 
-#get post
+#get blog
 @router.get("/blogs")
 def get_blogs(db: Session = Depends(get_db)):
     blog = db.query(Blog).all()
@@ -14,10 +14,45 @@ def get_blogs(db: Session = Depends(get_db)):
         return {"message": "blog does not exist"}
     return {"message": blog}
 
-#get post by id
+#get blog by id
 @router.get("/blogs/{id}")
 def get_blogs(id: int, db: Session = Depends(get_db)):
     blog = db.query(Blog).filter(Blog.id==id).all()
     if not blog:
         return {"message": "Blog does not exist"}
     return {"message": blog}
+
+# create blog
+@router.post("/blogs")
+def create_blogs(data: dict, db: Session = Depends(get_db)):
+    new_blog = Blog(**data)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+# Delete Blogs
+@router.post("/blogs/{id}")
+def delete_blogs(id: int, db: Session = Depends(get_db)):
+    blog = db.query(Blog).filter(Blog.id==id)
+    deleted_blog = blog.first()
+
+    if not deleted_blog:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+# Update Blog
+@router.put("/blogs/{id}")
+def update_blog(id: int, updated_blog: dict, db: Session = Depends(get_db)):
+    blog = db.query(Blog).filter(Blog.id == id)
+    new_blog = blog.first()
+
+    if not new_blog: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    
+    blog.update(updated_blog, synchronize_session=False)
+    db.commit()
+    return blog.first()
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,13 +1,49 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException, Response, status
 from sqlalchemy.orm import Session
 from ..database import get_db
 from ..models import User
 
 router = APIRouter()
 
+# get users
 @router.get("/users")
 def get_user(db : Session = Depends(get_db)):
     user = db.query(User).all()
     if not user: 
         return {"message": "user not found"}
     return {"data": user} 
+
+# create users
+@router.post("/users")
+def create_user(user_info: dict, db: Session = Depends(get_db)):
+    new_user = User(**user_info)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+# Deleting User
+@router.post("/users/{id}")
+def delete_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id==id)
+    deleted_user = user.first()
+
+    if not deleted_user: 
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    user.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+# Update USer
+@router.put("/users/{id}")
+def update_user(id: int, updated_user: dict, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id==id)
+    new_user = user.first()
+
+    if not new_user: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    
+    user.update(updated_user, synchronize_session=False)
+    db.commit()
+    return user.first()
